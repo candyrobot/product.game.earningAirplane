38a83dca87c21bfa6c609fe57713ddb948aa1406
田中 秀明 <tanaka_hideaki@aja-kk.co.jp>
2018-11-12 20:34:29 +0900

    update

--- reactredux/src/logic/index.js
+++ reactredux/src/logic/index.js
@@ -2,7 +2,10 @@ import Plane from './object/Plane';
 (function main() {
   // var el;
   // let plane = new Plane('1', el);
-  let plane = new Plane('1');
+  let plane = new Plane('1', {
+    course: new Map()
+  });
+  window.plane = plane;
   // plane.el();
   plane.runAndContinue().on('run', function(mile) {
     if(mile%15 === 0) {
--- reactredux/src/logic/object/Plane.js
+++ reactredux/src/logic/object/Plane.js
@@ -12,20 +12,26 @@
 export default class Plane {
 
   constructor(
+    arg,
+    option = {}
   ) {
     this._mile = 0;
     this._lv = 0;
     this._speed = 0;
     this._earningPower = 0;
+
+    this._timer = null;
   }
 
   runAndContinue() {
     let _run = function() {};
     let _eventName = '';
-    setInterval(()=> {
+
+    this._timer = setInterval(()=> {
       this.run();
       _run(this._mile);
     }, 100);
+
     return { on: function(eventName, fn) { _run = fn; _eventName = eventName } };
   }
 
@@ -35,7 +41,7 @@ export default class Plane {
   }
 
   stop() {
-
+    clearInterval(this._timer);
   }
 
 }
